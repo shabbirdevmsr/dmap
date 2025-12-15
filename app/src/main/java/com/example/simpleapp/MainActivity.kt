e253d936345c215da760ef0a3c0d54e7ed6a5ead
shabbirdevmsr <canvamse@gmail.com>
2025-12-15 21:13:36 +0600

    Add files via upload

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.simpleapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btn = findViewById<Button>(R.id.btnHello)
-        btn.setOnClickListener {
-            Toast.makeText(this, "Hello from GitHub Codespaces!", Toast.LENGTH_SHORT).show()
-        }
+        setContentView(android.R.layout.simple_list_item_1)
     }
 }
